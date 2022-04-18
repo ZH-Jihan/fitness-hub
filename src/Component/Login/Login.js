c288a16bb75f041ef06034f143c4c7271fba88bc
Zakir Hossain <zakirhossain19920@gmail.com>
2022-04-18 13:00:51 +0600

    add all socile

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,28 +1,52 @@
-import React from "react";
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
-import { Navigate } from "react-router-dom";
-import auth from '../../firebase.init';
+import React, { useRef } from "react";
+import {
+  useSendPasswordResetEmail,
+  useSignInWithEmailAndPassword,
+  useSignInWithFacebook,
+  useSignInWithGithub,
+  useSignInWithGoogle
+} from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
 import img from "../../Image/draw2.webp";
-import './Login.css';
+import "./Login.css";
 
 const Login = () => {
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-  
-  if (error) {
-    return (
+  const [signInWithGoogle, guser, gloading, gerror] = useSignInWithGoogle(auth);
+  const [signInWithGithub, gituser, gitloading, giterror] =useSignInWithGithub(auth);
+  const [signInWithFacebook, fbuser, fbloading, fberror] =useSignInWithFacebook(auth);
+  const [signInWithEmailAndPassword, user, loading, error] =useSignInWithEmailAndPassword(auth);
+  const [sendPasswordResetEmail, sending, sederror] =useSendPasswordResetEmail(auth);
+
+  const passwordRef = useRef("");
+  const emailRef= useRef("");
+
+  const handleSubmit = event =>{
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    signInWithEmailAndPassword(email,password)
+    
+  }
+
+  const navigate = useNavigate();
+  let eroreliment;
+  if (error || giterror || fberror || gerror) {
+    eroreliment = (
       <div>
-        <p>Error: {error.message}</p>
+        <p className="text-danger">Error: {error?.message || giterror?.message || fberror?.message || gerror?.message}</p>
       </div>
     );
   }
-  if (loading) {
-    return <p>Loading...</p>;
+  if (loading || gitloading || fbloading || gloading) {
+    <p>Loading...</p>;
   }
-  if (user) {
-    <Navigate to="/home" replace={true} />
-  };
+  if (user || gituser || fbuser || guser) {
+    navigate("/home");
+  }
   return (
-    <section >
+    <section>
       <div className="container-fluid h-custom my-5">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-md-9 col-lg-6 col-xl-5 py-3">
@@ -35,6 +59,7 @@ const Login = () => {
                 <button
                   type="button"
                   className="btn icon btn-floating mx-1"
+                  onClick={() => signInWithFacebook()}
                 >
                   <i className="fab fa-facebook-f"></i>
                 </button>
@@ -50,49 +75,38 @@ const Login = () => {
                 <button
                   type="button"
                   className="btn icon btn-floating mx-1"
+                  onClick={() => signInWithGithub()}
                 >
                   <i className="fab fa-github"></i>
                 </button>
               </div>
 
               <div className="divider d-flex align-items-center my-4">
-                  <div className="or-border"></div>
-                <p className="text-center justify-content-center  fw-bold mx-3 mb-0">Or</p>
+                <div className="or-border"></div>
+                <p className="text-center justify-content-center  fw-bold mx-3 mb-0">
+                  Or
+                </p>
                 <div className="or-border"></div>
               </div>
 
               {/* Email input */}
-
-              <div className="form-outline mb-4">
                 <input
+                  ref={emailRef}
                   type="email"
-                  id="form3Example3"
-                  className="form-control form-control-lg"
+                  name="Email"
+                  className="form-control form-control-lg mb-4"
                   placeholder="Enter a valid email address"
-                  required
+                  required 
                 />
-                <label className="form-label" for="form3Example3">
-                  Email address
-                </label>
-              </div>
-
               {/* <Password input  */}
-
-              <div className="form-outline mb-3">
-                <input
-                  type="password"
-                  id="form3Example4"
-                  className="form-control form-control-lg"
+                <input ref={passwordRef} type="password"
+                  className="form-control form-control-lg mb-4"
                   placeholder="Enter password"
-                  required
+                  required 
                 />
-                <label className="form-label" for="form3Example4">
-                  Password
-                </label>
-              </div>
+              {eroreliment}
               <div className="d-flex justify-content-between align-items-center">
                 {/* Checkbox  */}
-
                 <div className="form-check mb-0">
                   <input
                     className="form-check-input me-2"
@@ -104,7 +118,15 @@ const Login = () => {
                     Remember me
                   </label>
                 </div>
-                <a href="#!" className="text-body">
+                <a
+                  onClick={async () => {
+                    const email = emailRef.current.value;
+                    await sendPasswordResetEmail(email);
+                    alert("Sent email");
+                  }}
+                  href="#!"
+                  className="link-danger"
+                >
                   Forgot password?
                 </a>
               </div>
@@ -114,12 +136,15 @@ const Login = () => {
                   type="button"
                   className="btn login-btn btn-lg"
                   style={{ paddingleft: "2.5rem", paddingright: "2.5rem" }}
-                >LogIn</button>
+                  onClick={handleSubmit}
+                >
+                  LogIn
+                </button>
                 <p className="small fw-bold mt-2 pt-1 mb-0">
                   Don't have an account?{" "}
-                  <a href="#!" className="link-danger">
+                  <Link to="/singup" className="link-danger">
                     Register
-                  </a>
+                  </Link>
                 </p>
               </div>
             </form>
--- src/Component/SingUp/SingUp.js
+++ src/Component/SingUp/SingUp.js
@@ -2,17 +2,17 @@ import React from "react";
 
 const SingUp = () => {
   return (
-    <section class="">
+    <div>
       <div
-        class="px-4 py-5 px-md-5 text-center text-lg-start"
+        className="px-4 py-5 px-md-5 text-center text-lg-start"
         style={{ background: "hsl(0, 0%, 96%)" }}
       >
-        <div class="container">
-          <div class="row gx-lg-5 align-items-center">
-            <div class="col-lg-6 mb-5 mb-lg-0">
-              <h1 class="my-5 display-3 fw-bold ls-tight">
+        <div className="container">
+          <div className="row gx-lg-5 align-items-center">
+            <div className="col-lg-6 mb-5 mb-lg-0">
+              <h1 className="my-5 display-3 fw-bold ls-tight">
                 The best offer <br />
-                <span class="text-primary">for your business</span>
+                <span className="text-primary">for your business</span>
               </h1>
               <p style={{ color: "hsl(217, 10%, 50.8%)" }}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -22,32 +22,32 @@ const SingUp = () => {
               </p>
             </div>
 
-            <div class="col-lg-6 mb-5 mb-lg-0">
-              <div class="card">
-                <div class="card-body py-5 px-md-5">
+            <div className="col-lg-6 mb-5 mb-lg-0">
+              <div className="card">
+                <div className="card-body py-5 px-md-5">
                   <form>
                     {/* <!-- 2 column grid layout with text inputs for the first and last names --> */}
-                    <div class="row">
-                      <div class="col-md-6 mb-4">
-                        <div class="form-outline">
+                    <div className="row">
+                      <div className="col-md-6 mb-4">
+                        <div className="form-outline">
                           <input
                             type="text"
                             id="form3Example1"
-                            class="form-control"
+                            className="form-control"
                           />
-                          <label class="form-label" for="form3Example1">
+                          <label className="form-label" for="form3Example1">
                             First name
                           </label>
                         </div>
                       </div>
-                      <div class="col-md-6 mb-4">
-                        <div class="form-outline">
+                      <div className="col-md-6 mb-4">
+                        <div className="form-outline">
                           <input
                             type="text"
                             id="form3Example2"
-                            class="form-control"
+                            className="form-control"
                           />
-                          <label class="form-label" for="form3Example2">
+                          <label className="form-label" for="form3Example2">
                             Last name
                           </label>
                         </div>
@@ -56,41 +56,41 @@ const SingUp = () => {
 
                     {/* <!-- Email input --> */}
 
-                    <div class="form-outline mb-4">
+                    <div className="form-outline mb-4">
                       <input
                         type="email"
                         id="form3Example3"
-                        class="form-control"
+                        className="form-control"
                       />
-                      <label class="form-label" for="form3Example3">
+                      <label className="form-label" for="form3Example3">
                         Email address
                       </label>
                     </div>
 
                     {/* <!-- Password input --> */}
 
-                    <div class="form-outline mb-4">
+                    <div className="form-outline mb-4">
                       <input
                         type="password"
                         id="form3Example4"
-                        class="form-control"
+                        className="form-control"
                       />
-                      <label class="form-label" for="form3Example4">
+                      <label className="form-label" for="form3Example4">
                         Password
                       </label>
                     </div>
 
                     {/* <!-- Checkbox --> */}
 
-                    <div class="form-check d-flex justify-content-center mb-4">
+                    <div className="form-check d-flex justify-content-center mb-4">
                       <input
-                        class="form-check-input me-2"
+                        className="form-check-input me-2"
                         type="checkbox"
                         value=""
                         id="form2Example33"
                         checked
                       />
-                      <label class="form-check-label" for="form2Example33">
+                      <label className="form-check-label" for="form2Example33">
                         Subscribe to our newsletter
                       </label>
                     </div>
@@ -99,38 +99,38 @@ const SingUp = () => {
 
                     <button
                       type="submit"
-                      class="btn btn-primary btn-block mb-4"
+                      className="btn btn-primary btn-block mb-4"
                     >
                       Sign up
                     </button>
-                    <div class="text-center">
+                    <div className="text-center">
                       <p>or sign up with:</p>
                       <button
                         type="button"
-                        class="btn btn-link btn-floating mx-1"
+                        className="btn btn-link btn-floating mx-1"
                       >
-                        <i class="fab fa-facebook-f"></i>
+                        <i className="fab fa-facebook-f"></i>
                       </button>
 
                       <button
                         type="button"
-                        class="btn btn-link btn-floating mx-1"
+                        className="btn btn-link btn-floating mx-1"
                       >
-                        <i class="fab fa-google"></i>
+                        <i className="fab fa-google"></i>
                       </button>
 
                       <button
                         type="button"
-                        class="btn btn-link btn-floating mx-1"
+                        className="btn btn-link btn-floating mx-1"
                       >
-                        <i class="fab fa-twitter"></i>
+                        <i className="fab fa-twitter"></i>
                       </button>
 
                       <button
                         type="button"
-                        class="btn btn-link btn-floating mx-1"
+                        className="btn btn-link btn-floating mx-1"
                       >
-                        <i class="fab fa-github"></i>
+                        <i className="fab fa-github"></i>
                       </button>
                     </div>
                   </form>
@@ -140,7 +140,7 @@ const SingUp = () => {
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
